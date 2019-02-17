e74412ba3ac046fef6b26b03986064acb332123a
zero <yotiyotiarukinopengin@gmail.com>
2019-02-17 14:59:19 +0900

    Added password to database

--- CTFd/models.py
+++ CTFd/models.py
@@ -183,10 +183,15 @@ class Teams(db.Model):
 
 
 
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
-        #self.password = bcrypt_sha256.encrypt(str(password))
+    def __init__(self, name, email, password=None):
+        if password:
+            self.name = name
+            self.email = email
+            self.password = bcrypt_sha256.encrypt(str(password))
+        else:
+            self.name = name
+            self.email = email
+  
 
     def __repr__(self):
         return '<team %r>' % self.name
